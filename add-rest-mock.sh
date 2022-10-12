ea0723008e2028d3f611aa082184f41b43512126
Fabian Schlegel <fabian.schlegel@grayc.de>
2022-10-12 10:41:10 +0200

    Fix hard coded port in mock data script

--- add-rest-mock.sh
+++ add-rest-mock.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
-curl -X 'PUT' 'http://localhost:8080/mockserver/openapi' -H 'accept: application/json' \
+curl -X 'PUT' 'http://localhost:${API_PORT}/mockserver/openapi' -H 'accept: application/json' \
   -d "{ 'specUrlOrPayload' : '${MOCKDATA_PATH}/openapi.yaml' }" > /dev/null
